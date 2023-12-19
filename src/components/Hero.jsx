eb37d4af1a86f44e765d6507124a01f65ce25a51
Sameer Saleem <sameersaleemq75@gmail.com>
2023-12-19 13:03:57 +0500

    navbar-bug-fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <h1 className="text-white">HeroSection</h1>
+      <h1 className="text-white">
+        <div className="max-w-[800px] mt-[-96px] w-full text-center h-screen mx-auto flex flex-col justify-center">
+          <p className="uppercase text-[#00df9a] font-bold p-2">
+            growing with data analytics
+          </p>
+          <h1 className="uppercase md:text-7xl sm:text-6xl text-4xl font-bold sm:py-6 ">
+            grow with data.
+          </h1>
+          <div>
+            <p className="md:text-5xl sm:text-4xl text-xl font-bold ">
+              Fast, flexible financing for
+            </p>
+          </div>
+        </div>
+      </h1>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,35 +9,37 @@ function Navbar() {
   };
 
   return (
-    <div className="text-white flex justify-between h-24 max-w-[900px] mx-auto px-4 items-center">
+    <div className="text-white flex justify-between h-24 max-w-[900px]  mx-auto px-4 items-center">
       <h1 className="w-full font-bold text-3xl text-[#00df9a]">React.</h1>
       <ul className="hidden md:flex">
-        <li className="p-4">Home</li>
+        <li className="p-4">Home </li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav} className="block md:hidden">
+      <div onClick={handleNav} className=" block md:hidden">
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-      </div>
 
-      <div
-        className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300]"
-            : " fixed left-[-100%] ease-linear duration-500 "
-        }
-      >
-        <h1 className="w-full font-bold text-3xl text-[#00df9a] m-4">React.</h1>
+        <div
+          className={
+            !nav
+              ? "fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300]"
+              : " fixed left-[-100%] ease-in-out duration-500 "
+          }
+        >
+          <h1 className="w-full font-bold text-3xl text-[#00df9a] m-4">
+            React.
+          </h1>
 
-        <ul className=" p-3 uppercase">
-          <li className="p-4 border-b border-b-gray-600 ">Home</li>
-          <li className="p-4 border-b border-b-gray-600 ">Company</li>
-          <li className="p-4 border-b border-b-gray-600 ">Resources</li>
-          <li className="p-4 border-b border-b-gray-600 ">About</li>
-          <li className="p-4">Contact</li>
-        </ul>
+          <ul className=" p-3 uppercase">
+            <li className="p-4 border-b border-b-gray-600 ">Home</li>
+            <li className="p-4 border-b border-b-gray-600 ">Company</li>
+            <li className="p-4 border-b border-b-gray-600 ">Resources</li>
+            <li className="p-4 border-b border-b-gray-600 ">About</li>
+            <li className="p-4">Contact</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
